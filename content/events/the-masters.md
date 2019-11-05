fe756f93e91b7bb98b4f2ae0d87a71dae651d7cf
Jorge Mir <jorgemir@gmail.com>
2019-11-04 18:05:13 -0800

    first static build and export

---
+++ content/events/the-masters.md
@@ -0,0 +1,11 @@
+---
+title: The Masters
+date: '2019-01-28T18:44:37-08:00'
+lang: en
+tags:
+thumbnail: /images/uploads/the-masters.jpeg
+category: golf
+eventdate: 'Thursday, April 9 2020 - Sunday, April 12 2020'
+---
+
+The 2020 Masters tournament will take place on the second week of April at the Augusta National Golf Club. Contact us for corporate or group rates on exclusive properties in Augusta, Georgia.  
--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const withCSS = require('@zeit/next-css')
-const jdown = require('jdown') 
 
 module.exports = withCSS({
   webpack(config, options) {
@@ -10,27 +9,5 @@ module.exports = withCSS({
         }
     )
     return config
-  },
-  exportPathMap: async function() {
-    // pages we know about beforehand
-    const paths = {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/contact': { page: '/contact' }
-    }
-    // dynamic, data-generated pages
-    const content = await jdown('content') // assumes some markdown files in a `/content` folder, with frontmatter that offers a slug
-    const posts = [] // build up array of objects for the top level list
-    Object.entries(content).forEach(([filename, fileContent]) => {
-      
-      // the filename becomes the slug
-      paths[`/hotel-rooms/${filename}`] = { page: '/hotel-rooms/[slug]', query: { 
-          slug: filename, 
-          ...fileContent 
-        } 
-      }
-    })
-    console.log('paths: ', paths);
-    return paths
   }
 })
